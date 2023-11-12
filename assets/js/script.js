d10d5c8eb4baed81705e9d062d33923ec7df227c
github.com:sarahbryant411/portfolio-html-css.git <sarahbryant411>
2023-11-11 19:21:38 -0500

    added javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,46 @@
+var startBtn = document.querySelector("#start-btn");
+var quizStart = document.querySelector('.quiz-start');
+var quizScreen = document.querySelector(".quiz-screen")
 
+startBtn.addEventListener("click", startGame)
 
+var questions = [
+    {
+        prompt: "What color is the sky?",
+        options: {
+            a: "gray",
+            b: "green",
+            c: "chatreuse",
+            d: "blue",
+        },
+        correct: "d"
+    }, {
+        prompt: "What season are we in?",
+        options: {
+            a: "winter",
+            b: "spring",
+            c: "fall",
+            d: "summer",
+        },
+        correct: "c"
+    }, {
+        prompt: "What is thhe newest Iphone?",
+        options: {
+            a: "iphone 15",
+            b: "iphone 13",
+            c: "iphone 14",
+            d: "iphone 11",
+        },
+        correct: "a"
+    }
+]
 
+function startGame() {
+    console.log("start the game!")
 
-
-
+    quizStart.classList.add("hidden")
+    quizScreen.classList.remove("hidden")
+    //hide the quiz-start
+    // show the quiz-screen
+    //start the timer
+}
