fb924b8d33bb1f64f8dd79ce38778b4958e8ab40
github.com:sarahbryant411/portfolio-html-css.git <sarahbryant411>
2023-11-11 20:25:28 -0500

    worked on timer and correct answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,13 @@ var quizScreen = document.querySelector(".quiz-screen")
 var endScreen = document.querySelector(".end-screen")
 var optionButtons = document.querySelector("#option-buttons")
 var promptEl = document.querySelector("#prompt")
+var timeDisplay = document.querySelector("#time-display")
 
 startBtn.addEventListener("click", startGame)
 
 var currentQuestion = 0;
+var timeRemaining = 60;
+var timer;
 
 var questions = [
     {
@@ -45,15 +48,27 @@ function startGame() {
 
     quizStart.classList.add("hidden")
     quizScreen.classList.remove("hidden")
+ 
+    timeDisplay.textContent = timeRemaining;
+
+    // start the timer
+    timer = setInterval(() => {
+        timeRemaining--;
+
+        if (timeRemaining <= 0) {
+            timeDisplay.textContent = 0;
+            endGame()
+        } else timeDisplay.textContent = timeRemaining
+
+    }, 1000)
+
     renderQuestion()
-    //hide the quiz-start
-    // show the quiz-screen
-    //start the timer
+
 }
 
 function renderQuestion() {
     promptEl.textContent = questions[currentQuestion].prompt
-   
+
     var options = questions[currentQuestion].options
 
     optionButtons.innerHTML = ""
@@ -62,19 +77,25 @@ function renderQuestion() {
         var li = document.createElement("li")
         var button = document.createElement("button")
         button.textContent = options[letter]
-        button.addEventListener("click", ()=>{
+        button.setAttribute("data-letter", letter)
+        button.addEventListener("click", (e) => {
+
+            // correct or incorrect ?
+            console.log(e.target.dataset.letter === questions[currentQuestion].correct)
+
             currentQuestion++
-            if (currentQuestion>=questions.length){
+            if (currentQuestion >= questions.length) {
                 endGame()
             } else renderQuestion()
         })
         li.append(button)
         optionButtons.append(li)
     }
-    
+
 }
 
-function endGame(){
+function endGame() {
+    clearInterval(timer)
     //hide the quiz-screen
     quizScreen.classList.add("hidden")
     // show the end-screen
