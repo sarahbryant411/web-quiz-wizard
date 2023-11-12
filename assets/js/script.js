102f3738e11191b8dc7ef2c178ab74e52cb39c1f
github.com:sarahbryant411/portfolio-html-css.git <sarahbryant411>
2023-11-11 19:57:19 -0500

    added end screen hidden on html and the functions to go with them on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 var startBtn = document.querySelector("#start-btn");
 var quizStart = document.querySelector('.quiz-start');
 var quizScreen = document.querySelector(".quiz-screen")
+var endScreen = document.querySelector(".end-screen")
 var optionButtons = document.querySelector("#option-buttons")
+var promptEl = document.querySelector("#prompt")
 
 startBtn.addEventListener("click", startGame)
 
@@ -27,7 +29,7 @@ var questions = [
         },
         correct: "c"
     }, {
-        prompt: "What is thhe newest Iphone?",
+        prompt: "What is the newest Iphone?",
         options: {
             a: "iphone 15",
             b: "iphone 13",
@@ -50,13 +52,31 @@ function startGame() {
 }
 
 function renderQuestion() {
+    promptEl.textContent = questions[currentQuestion].prompt
+   
     var options = questions[currentQuestion].options
+
+    optionButtons.innerHTML = ""
 
     for (letter in options) {
         var li = document.createElement("li")
-        li.innerHTML = `<button>${options[letter]}</button>`
+        var button = document.createElement("button")
+        button.textContent = options[letter]
+        button.addEventListener("click", ()=>{
+            currentQuestion++
+            if (currentQuestion>=questions.length){
+                endGame()
+            } else renderQuestion()
+        })
+        li.append(button)
         optionButtons.append(li)
     }
-
     
+}
+
+function endGame(){
+    //hide the quiz-screen
+    quizScreen.classList.add("hidden")
+    // show the end-screen
+    endScreen.classList.remove("hidden")
 }
